719dd3b3e1669f20aefcc0f7b15d02d411a2ae40
FlyingArowana <AugustoMendes4426@gmail.com>
2025-01-27 23:58:19 +0000

    Added faction Bitmaks

--- src/tsg_qg_script.cpp
+++ src/tsg_qg_script.cpp
@@ -1,6 +1,9 @@
 
 enum Misc {
     NPC_QUESTGIVER = 99950,
+    FACTION_ALL = 0, // All Classes
+    FACTION_HORDE = 690, // All Horde Classes
+    FACTION_ALLIANCE = 1101, // All Aliiance Classes
     MAP_DEADMINES = 36, // Alliance Only Quests
     MAP_RFC = 389, // Horde Only Quests
     MAP_SCHOLOMANCE = 289; // 1x Alliance and Horde Only Quest, Rest both can do
