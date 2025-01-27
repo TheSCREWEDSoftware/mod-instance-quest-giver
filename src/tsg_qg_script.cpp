9b16a65f622db4eef8b2da28d72bc2b4fe85ebe6
FlyingArowana <AugustoMendes4426@gmail.com>
2025-01-27 23:41:31 +0000

    Added MAP_SCHOLOMANCE  for example on discord

--- src/tsg_qg_script.cpp
+++ src/tsg_qg_script.cpp
@@ -1,8 +1,9 @@
 
 enum Misc {
     NPC_QUESTGIVER = 99950,
-    MAP_DEADMINES = 36,
-    MAP_RFC = 389;
+    MAP_DEADMINES = 36, // Alliance Only Quests
+    MAP_RFC = 389, // Horde Only Quests
+    MAP_SCHOLOMANCE = 289; // 1x Alliance and Horde Only Quest, Rest both can do
 };
 
 
